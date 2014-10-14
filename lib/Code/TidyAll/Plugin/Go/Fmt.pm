00307315530092858b09cae30b7ca7317e2cc421
MaxMind, Inc. <support@maxmind.com>
2014-10-14 20:15:48 +0000

    Import of MAXMIND/Code-TidyAll-Plugin-Go-0.01 from CPAN.
    gitpan-cpan-distribution: Code-TidyAll-Plugin-Go
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MAXMIND/Code-TidyAll-Plugin-Go-0.01.tar.gz
    gitpan-cpan-author:       MAXMIND
    gitpan-cpan-maturity:     released

---
+++ lib/Code/TidyAll/Plugin/Go/Fmt.pm
@@ -0,0 +1,36 @@
+package Code::TidyAll::Plugin::Go::Fmt;
+$Code::TidyAll::Plugin::Go::Fmt::VERSION = '0.01';
+use strict;
+use warnings;
+
+use Capture::Tiny qw( capture_merged );
+use Moo;
+use Try::Tiny;
+
+extends 'Code::TidyAll::Plugin';
+
+sub _build_cmd { 'gofmt' }
+
+sub transform_file {
+    my ( $self, $file ) = @_;
+
+    try {
+        my $cmd = join q{ }, $self->cmd, $self->argv, $file;
+        my $output = capture_merged { system($cmd) and die "$cmd failed"; };
+        _write_file( $file, $output );
+    }
+    catch {
+        die sprintf(
+            q{%s exited with error - possibly bad arg list '%s'},
+            $self->cmd, $self->argv
+        );
+    };
+}
+
+sub _write_file {
+    my ( $file, $contents ) = @_;
+    open( my $fh, ">", $file ) or die "could not open $file: $!";
+    print $fh $contents;
+}
+
+1;
